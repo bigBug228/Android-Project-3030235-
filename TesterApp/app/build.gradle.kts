a4c9a48ea2f503d4887aebdfd2144a95dce5cfe0
Ivan Zhukov <ivan.zhukov@student.griffith.ie>
2023-11-10 12:30:08 +0000

    addImplementationsForTestActivity

--- TesterApp/app/build.gradle.kts
+++ TesterApp/app/build.gradle.kts
@@ -50,7 +50,9 @@ android {
 }
 
 dependencies {
-
+    implementation ("nl.dionsegijn:konfetti-compose:2.0.3")
+    implementation("androidx.navigation:navigation-compose:2.4.0-beta01")
+    implementation ("androidx.appcompat:appcompat:1.4.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
