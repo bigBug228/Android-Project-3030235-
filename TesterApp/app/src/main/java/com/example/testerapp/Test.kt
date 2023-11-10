e2d86596bec42d69063ad8a414471be15135e475
Ivan Zhukov <ivan.zhukov@student.griffith.ie>
2023-11-10 12:49:36 +0000

    addStringQuestionScreen

--- TesterApp/app/src/main/java/com/example/testerapp/Test.kt
+++ TesterApp/app/src/main/java/com/example/testerapp/Test.kt
@@ -25,9 +25,11 @@ import androidx.compose.material3.TextField
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.ui.Alignment
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 import nl.dionsegijn.konfetti.compose.KonfettiView
@@ -36,6 +38,15 @@ import nl.dionsegijn.konfetti.core.Position
 import nl.dionsegijn.konfetti.core.emitter.Emitter
 import java.util.concurrent.TimeUnit
 
+var score =0
+val BlueGreenGradientStart = Color(0xFF0081A7)
+val BlueGreenGradientEnd = Color(0xFF00BFA5)
+val AccentColor = Color(0xFFFF6B6B)
+val NeutralColor = Color(0xFFF0F0F0)
+val DarkTextColor = Color(0xFF002D62)
+
+// Secondary color for text and button
+val LightTextColor = Color(0xFFFFFFFF)
 class Test : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,6 +66,74 @@ fun NavigationScreen(color:String) {
         backgroundColor = Color(0xFF0081A7)
     }
     Surface(modifier = Modifier.fillMaxSize(),color = backgroundColor) {
+        NavHost(navController = navController, startDestination = "question/1"){
+            composable("question/1"){
+                StringQuestionScreen(navController = navController)
+            }
+            composable("question/2"){
+                //CheckboxQuestionScreen(navController = navController)
+            }
+            composable("question/3"){
+                //RadioQuestionScreen(navController = navController)
+            }
+            composable("question/4"){
+                //ResultScreen(navController = navController)
+            }
 
+        }
+
+    }
+}
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun StringQuestionScreen(navController: NavHostController) {
+    var userAnswer by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = "What is the capital of France?",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = LightTextColor
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = userAnswer,
+            onValueChange = { userAnswer = it },
+            label = { Text("Your Answer",color = LightTextColor) },
+            modifier = Modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = BlueGreenGradientEnd, // Use containerColor for Material 3
+                textColor = LightTextColor,
+                cursorColor = LightTextColor,
+                // You might also want to set other colors like focusedLabelColor, unfocusedLabelColor etc.
+            )
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(
+                onClick = {
+                    if(userAnswer.equals("Paris",ignoreCase = true)){
+                        score +=1
+                    }
+                    navController.navigate("question/2")
+                },
+                modifier = Modifier.padding(16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = BlueGreenGradientEnd // Use containerColor for the button background
+                )
+            ) {
+                Text("Next")
+            }
+        }
     }
 }
