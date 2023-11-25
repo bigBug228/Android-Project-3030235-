190b169b7c5b4517990e53b1b6e1858ba7d6d333
Ivan Zhukov <ivan.zhukov@student.griffith.ie>
2023-11-25 03:00:37 +0000

    moreQuestionsInTest

--- TesterApp/app/src/main/java/com/example/testerapp/Test.kt
+++ TesterApp/app/src/main/java/com/example/testerapp/Test.kt
@@ -42,6 +42,9 @@ import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.RadioButtonDefaults
 import android.content.Intent
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.size
 //create variable to count correct answers
 var score =0
 var isAnswered = false
@@ -88,12 +91,24 @@ fun NavigationScreen(color:String,name:String) {
                 StringQuestionScreen(navController = navController)
             }
             composable("question/2"){
-                CheckboxQuestionScreen(navController = navController)
+                StringTwoQuestionScreen(navController = navController)
             }
             composable("question/3"){
-                RadioQuestionScreen(navController = navController)
+                CheckboxQuestionScreen(navController = navController)
             }
             composable("question/4"){
+                CheckboxTwoQuestionScreen(navController = navController)
+            }
+            composable("question/5"){
+                RadioQuestionScreen(navController = navController)
+            }
+            composable("question/6"){
+                RadioTwoQuestionScreen(navController = navController)
+            }
+            composable("question/7"){
+                RadioImageQuestionScreen(navController = navController)
+            }
+            composable("question/8"){
                 ResultScreen(navController = navController,name)
             }
 
@@ -147,6 +162,65 @@ fun StringQuestionScreen(navController: NavHostController) {
                         score +=1
                     }
                     navController.navigate("question/2")
+                },
+                modifier = Modifier.padding(16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = BlueGreenGradientEnd // Use containerColor for the button background
+                )
+            ) {
+                Text("Next")
+            }
+        }
+    }
+}
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun StringTwoQuestionScreen(navController: NavHostController) {
+    // create variable to store user answer
+    var userAnswer by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = "What is the name of the physical phenomenon " +
+                    "that leads to attraction between all particles with mass " +
+                    "and is the reason why objects fall downwards on Earth?",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = LightTextColor
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            //user will use this text field to answer question
+            value = userAnswer,
+            onValueChange = { userAnswer = it },
+            label = { Text("Your Answer",color = LightTextColor) },
+            modifier = Modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = BlueGreenGradientEnd,
+                textColor = LightTextColor,
+                cursorColor = LightTextColor,
+
+                )
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            //button to move to the next question
+            Button(
+                onClick = {
+                    //check user answer, if its correct, add 1 to score value
+                    if(userAnswer.equals("gravity",ignoreCase = true)){
+                        score +=1
+                    }
+                    navController.navigate("question/3")
                 },
                 modifier = Modifier.padding(16.dp),
                 colors = ButtonDefaults.buttonColors(
@@ -219,7 +293,80 @@ fun CheckboxQuestionScreen(navController: NavHostController) {
                     if(selectedOptions.size==6){
                         score+=1
                     }
-                    navController.navigate("question/3")
+                    navController.navigate("question/4")
+                },
+                modifier = Modifier.padding(16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = BlueGreenGradientEnd // Use containerColor for the button background
+                )
+            ) {
+                Text("Next")
+            }
+        }
+    }
+}
+@Composable
+fun CheckboxTwoQuestionScreen(navController: NavHostController) {
+    //list of all options for the user
+    val options = listOf("Cats", "Foxes", "Dogs", "Rats", "Raсoon", "Squirrel")
+    //create list of options which user selected
+    val selectedOptions = remember { mutableStateListOf<String>() }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        //text to specify task for the user
+        Text(
+            text = "Select the animals which can be pets.",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = LightTextColor
+
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        //for each option from the list create checkbox with text
+        options.forEach { option ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Checkbox(
+                    checked = selectedOptions.contains(option),
+                    //if user presses checkbox, this answer will be added to selected options list
+                    onCheckedChange = { checked ->
+                        if (checked) {
+                            selectedOptions.add(option)
+                        } else {
+                            selectedOptions.remove(option)
+                        }
+                    },
+                    modifier = Modifier.padding(8.dp),
+                    colors = CheckboxDefaults.colors(
+                        checkedColor = BlueGreenGradientEnd,
+                        uncheckedColor = BlueGreenGradientEnd.copy(alpha = 0.6f),
+                        checkmarkColor = LightTextColor
+                    ),
+                )
+                Text(text = option,color = LightTextColor)
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            //button to move to the next question
+            Button(
+                //if selected options list has a size of six, score value will be +1
+                onClick = {
+                    if(selectedOptions.contains("Cats") and selectedOptions.contains("Dogs")){
+                        score+=1
+                    }
+                    navController.navigate("question/5")
                 },
                 modifier = Modifier.padding(16.dp),
                 colors = ButtonDefaults.buttonColors(
@@ -283,7 +430,7 @@ fun RadioQuestionScreen(navController: NavHostController) {
                     if(selectedOption.equals("Mars",ignoreCase = true)){
                         score+=1
                     }
-                    navController.navigate("question/4")
+                    navController.navigate("question/6")
                 },
                 modifier = Modifier.padding(16.dp),
                 colors = ButtonDefaults.buttonColors(
@@ -295,6 +442,146 @@ fun RadioQuestionScreen(navController: NavHostController) {
         }
     }
 }
+@Composable
+fun RadioTwoQuestionScreen(navController: NavHostController) {
+    //list of all options
+    val options = listOf("USA", "UK", "Ireland", "France")
+    //option which user selected
+    var selectedOption by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        //text which will specify the task for the user
+        Text(
+            text = "In which country was the Statue of Liberty built?",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = LightTextColor
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        //for each option program creates radio button with text
+        options.forEach { option ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                RadioButton(
+                    selected = selectedOption == option,
+                    onClick = { selectedOption = option },
+                    modifier = Modifier.padding(8.dp),
+                    colors = RadioButtonDefaults.colors(
+                        selectedColor = BlueGreenGradientEnd,
+                        unselectedColor = BlueGreenGradientEnd.copy(alpha = 0.6f)
+                    ),
+                )
+                Text(text = option,color = LightTextColor)
+            }
+        }
+
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            //button to move to result screen
+            Button(
+                onClick = {
+                    //if selected option equals to Mars, score + 1
+                    if(selectedOption.equals("France",ignoreCase = true)){
+                        score+=1
+                    }
+                    navController.navigate("question/7")
+                },
+                modifier = Modifier.padding(16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = BlueGreenGradientEnd // Use containerColor for the button background
+                )
+            ) {
+                Text("Next")
+            }
+        }
+    }
+}
+@Composable
+fun RadioImageQuestionScreen(navController: NavHostController) {
+    //list of all options
+    val optionsWithImages = listOf(
+        "USA" to R.drawable.usa,
+        "UK" to R.drawable.uk,
+        "Ireland" to R.drawable.ireland,
+        "France" to R.drawable.france
+    )
+    //option which user selected
+    var selectedOption by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        //text which will specify the task for the user
+        Text(
+            text = "Show the flag of Ireland",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = LightTextColor
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        //for each option program creates radio button with text
+        optionsWithImages.forEach { (option, imageRes) ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                RadioButton(
+                    selected = selectedOption == option,
+                    onClick = { selectedOption = option },
+                    modifier = Modifier.padding(8.dp),
+                    colors = RadioButtonDefaults.colors(
+                        selectedColor = BlueGreenGradientEnd,
+                        unselectedColor = BlueGreenGradientEnd.copy(alpha = 0.6f)
+                    ),
+                )
+                Image(
+                    painter = painterResource(id = imageRes),
+                    contentDescription = "Image for $option",
+                    modifier = Modifier.size(40.dp) // Set the size as needed
+                )
+            }
+        }
+
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            //button to move to result screen
+            Button(
+                onClick = {
+                    //if selected option equals to France, score + 1
+                    if(selectedOption.equals("Ireland",ignoreCase = true)){
+                        score+=1
+                    }
+                    navController.navigate("question/8")
+                },
+                modifier = Modifier.padding(16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = BlueGreenGradientEnd // Use containerColor for the button background
+                )
+            ) {
+                Text("Next")
+            }
+        }
+    }
+}
+
 @Composable
 fun ResultScreen(navController: NavHostController,name: String) {
     //context for intent which will start menu activity
@@ -314,24 +601,25 @@ fun ResultScreen(navController: NavHostController,name: String) {
             color = LightTextColor,
             modifier = Modifier.align(Alignment.CenterHorizontally) // Center horizontally in the Column
         )
-
-        //design element which created konfetti effect at the center of result screen for a few seconds
-        KonfettiView(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxWidth(),
-            parties = listOf(
-                Party(
-                    speed = 4f,
-                    maxSpeed = 30f,
-                    damping = 0.9f,
-                    spread = 360,
-                    colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
-                    position = Position.Relative(0.5, 0.5), // Centered
-                    emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(100)
+        if (score==7) {
+            //design element which created konfetti effect at the center of result screen for a few seconds
+            KonfettiView(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+                parties = listOf(
+                    Party(
+                        speed = 4f,
+                        maxSpeed = 30f,
+                        damping = 0.9f,
+                        spread = 360,
+                        colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
+                        position = Position.Relative(0.5, 0.5), // Centered
+                        emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(100)
+                    )
                 )
             )
-        )
+        }
         Spacer(Modifier.weight(1f))
         //button which sets score back to zero and brings user back to menu activity. Can start test again
         Button(
@@ -354,3 +642,4 @@ fun ResultScreen(navController: NavHostController,name: String) {
         }
     }
 }
+
